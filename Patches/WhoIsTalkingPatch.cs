bdcf0a64865dd7abe1faf3a5a2f216d03e3d9959
HanSolo1000Falcon <202703912+HanSolo1OOOFalcon@users.noreply.github.com>
2025-04-06 21:57:04 +0200

    Add files via upload

---
+++ Patches/WhoIsTalkingPatch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HarmonyLib;
+using UnityEngine;
+using WhoIsTalking;
+namespace WhoIsSpeeding.Patches
+{
+    [HarmonyPatch(typeof(NameTagHandler))]
+    [HarmonyPatch("GetInfo", MethodType.Normal)]
+    internal class WhoIsTalkingPatch0
+    {
+        private static void Postfix(NameTagHandler __instance)
+        {
+            __instance.GetOrAddComponent<VelocityHandler>(out var NTH);
+            NTH.nameTagHandler = __instance;
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using BepInEx;
+using UnityEngine;
+using Utilla;
+using WhoIsSpeeding;
+using WhoIsTalking;
+
+namespace WhoIsSpeeding
+{
+    // This is your mod's main class.
+    [BepInDependency("Graze.WhoIsTalking")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        void Start()
+        {
+            HarmonyPatches.ApplyHarmonyPatches();
+        }
+    }
+}
